dfe198998cbf1bedc373a646cba9378f61220b68
AngelaTsen <125123519+AngelaTsen@users.noreply.github.com>
2023-05-08 11:10:32 +0300

    add ajax

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-fetch('https://api.themoviedb.org/3/search/movie?api_key=684ecd12253bde93eb817856fa49084b&query=batman')
-.then(response => response.json())
-.then(data => {
-  console.log(data);
-  
+
+const inputSearch = document.getElementById('input-search');
+const buttonSearch = document.getElementById('search');
+
+buttonSearch.addEventListener('click', () => {
+    const filmQuery = inputSearch.value;
+    console.log(filmQuery);
+    const divElement = document.getElementById('container');
+    divElement.innerHTML = '';
+
+fetch('https://api.themoviedb.org/3/search/movie?api_key=684ecd12253bde93eb817856fa49084b&query='+filmQuery)
+    .then(response => response.json())
+    .then(data => {
+        if(data.results.lenght === undefined){
+        divElement.innerHTML = 'Нічого не знайдено :(';
+        }
+
+console.log(data);
+data.results.forEach(function(film) {
+        console.log(film.title);
+        const newElement = document.createElement('div'); 
+        newElement.textContent = film.title; 
+        divElement.appendChild(newElement)
+        });
 })
-.catch(error => {
-  console.error('Сталася помилка:', error);
-});
+    .catch(error => {
+      console.error('Сталася помилка:', error);
+    });
 
 
-fetch(apiURL)
-  .then(response => response.json())
-  .then((data) => data);
-  
-  const postsWrapper = document.body.querySelector('#postsWrapper');
-
+  });
